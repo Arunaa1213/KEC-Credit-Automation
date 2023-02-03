2acfc58e69b882abb4003d712cbf6a8ae4857dcf
Gokulkrishna Raju <gmkrishna11082003@gmail.com>
2023-02-03 21:04:42 +0530

    Updates App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {
 } from "react-router-dom";
 
 import Home from "./Home";
+import AddCourse from "./staff/course/AddCourse";
 import StaffLogin from "./staff/login/StaffLogin";
 import StaffHome from "./staff/StaffHome";
 import StudentLogin from "./student/login/StudentLogin";
@@ -16,29 +17,22 @@ function App() {
       <HashRouter>
         <Routes>
           <Route path="/" element={
-            <div>
               <Home />
-            </div>
           } />
           <Route path="/staffhome" element={
-            <div>
               <StaffHome />
-            </div>
           } />
           <Route path="/studenthome" element={
-            <div>
               <StudentHome />
-            </div>
           } />
           <Route path="/stafflogin" element={
-            <div>
               <StaffLogin />
-            </div>
           } />
           <Route path="/studentlogin" element={
-            <div>
               <StudentLogin />
-            </div>
+          } />
+          <Route path="/addcourse" element={
+              <AddCourse />
           } />
         </Routes>
       </HashRouter>
