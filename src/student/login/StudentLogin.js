c682832f4f73f83d48b74d7dde6f928257e0205d
Arunaa S <arunaasureshkumar@gmail.com>
2023-02-05 10:21:00 +0530

    updated student login

--- src/student/login/StudentLogin.js
+++ src/student/login/StudentLogin.js
@@ -60,7 +60,6 @@ function StudentLogin() {
               type="text"
               onChange={handleName}
               className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
-              onChange={nameHandler}
             />
           </div>
           <div className="mb-2">
@@ -75,14 +74,16 @@ function StudentLogin() {
               type="password"
               onChange={handlePass}
               className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
-              onChange={passwordHandler}
             />
           </div>
           <a href="#" className="text-xs text-purple-600 hover:underline">
             Forget Password?
           </a>
           <div className="mt-6">
-            <button type="submit" className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600">
+            <button
+              type="submit"
+              className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600"
+            >
               Login
             </button>
           </div>
@@ -98,6 +99,6 @@ function StudentLogin() {
       </div>
     </div>
   );
-  }
+}
 
 export default StudentLogin;
