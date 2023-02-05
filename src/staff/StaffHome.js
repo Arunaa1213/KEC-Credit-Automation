b1c5ecd9f37606b9a17815c2193bbf805b51daf6
CHANDRAMUKHII <chandramukhiianbarasu@gmail.com>
2023-02-05 19:21:43 +0530

    Routing

--- src/staff/StaffHome.js
+++ src/staff/StaffHome.js
@@ -1,7 +1,17 @@
 import StaffNav from "../staff/StaffNav";
-
+import { Routes, Route, HashRouter, Link } from "react-router-dom";
+import AddCourse from "./course/AddCourse";
+import AddNPTELCourse from "../student/course/AddNPTELCourse";
 function StaffHome() {
-  return <StaffNav />;
+  return (
+    <div>
+      <Link to="/addcourse">Add course</Link>
+
+      <Routes>
+        <Route path="/addcourse" element={<AddCourse />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default StaffHome;
--- src/student/StudentHome.js
+++ src/student/StudentHome.js
@@ -1,6 +1,22 @@
+import { Routes, Route, HashRouter, Link } from "react-router-dom";
 import StudentNav from "../student/StudentNav";
+import AddNPTELCourse from "./course/AddNPTELCourse";
+import OptCourse from "./course/OptCouse";
+import Exemption from "./Exemption";
 function StudentHome() {
-  return <StudentNav />;
+  return (
+    <div>
+      <StudentNav />
+      <Link to="/addnptelcourse">Add NPTEL Course</Link>
+      <Link to="/optcourse">Opt course</Link>
+      <Link to="/exemption">Exemption</Link>
+      <Routes>
+        <Route path="/addnptelcourse" element={<addNPTELCourse />} />
+        <Route path="/optcourse" element={<OptCourse />} />
+        <Route path="/exemption" element={<Exemption />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default StudentHome;
