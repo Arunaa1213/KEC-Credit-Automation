e4a3d20a211c7d96acdd71349ca972625eb15992
Arunaa S <arunaasureshkumar@gmail.com>
2023-02-05 23:07:32 +0530

    student login added

--- src/student/Exemption.js
+++ src/student/Exemption.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import axios from "axios";
-
+import { useNavigate } from "react-router-dom";
 function Exemption() {
   const navigate = useNavigate();
   const [inputs, setInputs] = useState([]);
--- src/student/login/StudentLogin.js
+++ src/student/login/StudentLogin.js
@@ -1,40 +1,63 @@
 import React, { useState } from "react";
 import axios from "axios";
-
 import { useNavigate } from "react-router-dom";
 function StudentLogin() {
   const navigate = useNavigate();
-  const [name, setName] = useState("");
-  const [password, setPassword] = useState("");
-  async function handleSubmit() {
-    if (name.length === 0) {
-      alert("name is empty");
-    } else if (password.length === 0) {
-      alert("password is empty");
-    } else {
-      const url = "http://localhost:81/credit_automation/studentLogin.php";
-      let fData = new FormData();
-      fData.append("name", name);
-      fData.append("password", password);
-      await axios
-        .post(url, fData)
-        .then((response) => {
-          console.log(response);
-          navigate("/StudentHome");
-        })
-        .catch((error) => alert(error));
-    }
-  }
-  const handleName = (event) => {
-    event.persist();
-    console.log(event.target.value);
-    setName(event.target.value);
+  // const [name, setName] = useState("");
+  // const [password, setPassword] = useState("");
+  // async function handleSubmit() {
+  //   if (name.length === 0) {
+  //     alert("name is empty");
+  //   } else if (password.length === 0) {
+  //     alert("password is empty");
+  //   } else {
+  //     const url = "http://localhost:81/credit_automation/studentLogin.php";
+  //     let fData = new FormData();
+  //     fData.append("name", name);
+  //     fData.append("password", password);
+  //     console.log(fData);
+  //     await axios
+  //       .post(url, fData)
+  //       .then((response) => {
+  //         console.log(response);
+  //         // if (response) {
+  //         //   navigate("/StudentHome");
+  //         // }
+  //       })
+  //       .catch((error) => alert(error));
+  //   }
+  // }
+  // const handleName = (event) => {
+  //   event.persist();
+  //   setName(event.target.value);
+  // };
+  // const handlePass = (event) => {
+  //   event.persist();
+  //   setPassword(event.target.value);
+  // };
+  const [inputs, setInputs] = useState([]);
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs((values) => ({ ...values, [name]: value }));
   };
-  const handlePass = (event) => {
-    event.persist();
-    console.log(event.target.value);
-    setPassword(event.target.value);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(inputs);
+    axios
+      .post("http://localhost:81/credit_automation/studentLogin.php", inputs)
+      .then(function (response) {
+        if (response.data.length === 0) {
+          alert("invalid");
+        } else {
+          console.log(response.data[0]["studentName"]);
+          navigate("/studenthome");
+        }
+      });
   };
+
   return (
     <div className="relative flex flex-col justify-center min-h-screen overflow-hidden">
       <div className="w-full p-6 m-auto bg-white rounded-md shadow-md lg:max-w-xl">
@@ -52,7 +75,7 @@ function StudentLogin() {
             <input
               name="username"
               type="text"
-              onChange={handleName}
+              onChange={handleChange}
               className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
             />
           </div>
@@ -66,7 +89,7 @@ function StudentLogin() {
             <input
               name="password"
               type="password"
-              onChange={handlePass}
+              onChange={handleChange}
               className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
             />
           </div>
