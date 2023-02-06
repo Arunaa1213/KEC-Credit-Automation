a6ed1ef555d63667e14c7814eea5672ce0b4d285
Arunaa S <arunaasureshkumar@gmail.com>
2023-02-06 09:59:45 +0530

    staff login added

--- src/staff/StaffHome.js
+++ src/staff/StaffHome.js
@@ -1,9 +1,11 @@
-import { Link } from "react-router-dom";
 import StaffNav from "./StaffNav";
-function StaffHome() {
+import { useLocation } from "react-router-dom";
+function StaffHome(state) {
+  const location = useLocation();
   return (
     <div>
       <StaffNav />
+      <p className="m-[200px]">helo {location.state.current_user}</p>
     </div>
   );
 }
--- src/student/StudentHome.js
+++ src/student/StudentHome.js
@@ -1,9 +1,12 @@
 import { Link } from "react-router-dom";
 import StudentNav from "../student/StudentNav";
-function StudentHome() {
+import { useLocation } from "react-router-dom";
+function StudentHome(state) {
+  const location = useLocation();
   return (
     <div>
       <StudentNav />
+      <p className="m-[200px]">helo {location.state.current_user}</p>
     </div>
   );
 }
