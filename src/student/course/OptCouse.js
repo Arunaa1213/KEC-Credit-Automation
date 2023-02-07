a36f992d6f199d7f6bd5ac309294460ca3204fcb
Gokulkrishna Raju <gmkrishna11082003@gmail.com>
2023-02-07 11:13:45 +0530

    Back Option enabled in Forms

--- src/student/course/OptCouse.js
+++ src/student/course/OptCouse.js
@@ -1,7 +1,9 @@
 import { useState , useEffect } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export default function OptCourse() {
+    const navigate = useNavigate();
 
 const [users, setUsers] = useState([]);
     useEffect(() => {
@@ -16,8 +18,12 @@ const [users, setUsers] = useState([]);
     }
 
     return (
-        <div>
-            <div className="flex flex-col items-center mt-4">
+        <div className="bg-pELC text-pC h-screen">
+            <button className="absolute left-12 top-6" onClick={() => navigate(-1)}> 
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" className="h-4 inline m-2"><path d="M41.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 256 278.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"/></svg>      
+            </button>
+            <div className="h-full">
+            <div className="flex flex-col items-center text-3xl font-extrabold pt-8 mb-4">
                 <h2>COLLEGE OFFERING COURSES</h2>
             </div>
             <table className="table-fixed w-11/12 border-separate border-spacing-1 text-center">
@@ -45,6 +51,7 @@ const [users, setUsers] = useState([]);
                     )}
               </tbody>
             </table>
+            </div>
         </div>
     );
 }
