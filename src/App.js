44211bb6e5a9a933d21379595994e76326486d61
Gokulkrishna Raju <gmkrishna11082003@gmail.com>
2023-02-09 16:12:56 +0530

    ExcemptionVerificationCard Styled

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import AddCourse from './staff/course/AddCourse'
 import StaffLogin from './staff/login/StaffLogin'
 import StaffHome from './staff/StaffHome'
 import AddNPTELCourse from './student/course/AddNPTELCourse'
-import OptCourse from './student/course/OptCouse'
 import StudentLogin from './student/login/StudentLogin'
 import StudentHome from './student/StudentHome'
 import Exemption from './student/course/Exemption'
 import AdminHome from './admin/AdminHome'
 import ExemptionVerification from './staff/course/ExemptionVerification'
 import ExcemptionVerificationCard from './staff/course/ExemptionVerificationCard'
+import OptCourse from './student/course/OptCourse'
 
 function App () {
   return (
