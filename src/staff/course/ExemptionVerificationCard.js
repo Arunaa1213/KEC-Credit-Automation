e6d3a9a551970ed7326519e38bf20b1ef0a2117f
Arunaa S <arunaasureshkumar@gmail.com>
2023-02-10 14:16:12 +0530

    Exemptioncard updated

--- src/staff/course/ExemptionVerificationCard.js
+++ src/staff/course/ExemptionVerificationCard.js
@@ -1,47 +1,19 @@
 import { useLocation } from "react-router-dom";
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import axios from "axios";
 
-function ExcemptionVerificationCard(state) {
+function ExcemptionVerificationCard() {
   const location = useLocation();
-  const navigate = useNavigate();
-  const [users, setUsers] = useState([]);
-  const inputs = { username: location.state.roll };
-
-  axios
-    .post(
-      "http://localhost:81/KEC-Credit-Automation-DB/getByRollNo.php",
-      inputs
-    )
-    .then(function(response) {
-      setUsers(response.data);
-      console.log(
-        "file: ExemptionVerificationCard.js:7 ~ ExcemptionVerificationCard ~ users",
-        users
-      );
-    });
 
   return (
-    <div className="w-screen h-screen p-6 rounded-md shadow-md bg-pELC flex flex-col justify-center items-center">
-      <button className="absolute left-12 top-6" onClick={() => navigate(-1)}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 384 512"
-          className="h-4 inline m-2"
-        >
-          <path d="M41.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 256 278.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z" />
-        </svg>
-      </button>
-      <div className="border border-pMC w-1/2">
+    <div className="w-screen p-6 rounded-md shadow-md bg-pELC flex flex-col justify-center items-center">
+      <div className="border border-pMC w-fit sm:max-w-1/2">
         <table className="w-full text-sm text-left text-pMC ">
           <tbody>
             <tr className="border-b border-pMC">
               <th
                 scope="row"
-                className=" px-6 py-4 font-medium text-pC whitespace-nowrap bg-pLC dark:text-white dark:bg-gray-800"
+                className=" px-6 py-4 font-medium text-pC whitespace-nowrap bg-pC dark:text-white dark:bg-gray-800"
               >
-                Name
+                Roll Number
               </th>
               <td className="px-6 py-4 uppercase">{location.state.roll}</td>
             </tr>
@@ -50,9 +22,20 @@ function ExcemptionVerificationCard(state) {
                 scope="row"
                 className="px-6 py-4 font-medium text-pC whitespace-nowrap bg-pLC dark:text-white dark:bg-gray-800"
               >
-                Roll Number
+                Name
               </th>
               <td className="px-6 py-4 uppercase">
+                {location.state.student_name}
+              </td>
+            </tr>
+            <tr className="border-b border-pMC">
+              <th
+                scope="row"
+                className="px-6 py-4 font-medium text-pC whitespace-nowrap bg-pLC dark:text-white dark:bg-gray-800"
+              >
+                Academic course name
+              </th>
+              <td className="px-6 py-4">
                 {location.state.academic_course_name}
               </td>
             </tr>
@@ -61,16 +44,7 @@ function ExcemptionVerificationCard(state) {
                 scope="row"
                 className="px-6 py-4 font-medium text-pC whitespace-nowrap bg-pLC dark:text-white dark:bg-gray-800"
               >
-                Email
-              </th>
-              <td className="px-6 py-4">{location.state.semester}</td>
-            </tr>
-            <tr className="border-b border-pMC">
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium text-pC whitespace-nowrap bg-pLC dark:text-white dark:bg-gray-800"
-              >
-                Department
+                Type of course
               </th>
               <td className="px-6 py-4 uppercase">
                 {location.state.type_of_course}
@@ -81,7 +55,7 @@ function ExcemptionVerificationCard(state) {
                 scope="row"
                 className="px-6 py-4 font-medium text-pC whitespace-nowrap bg-pLC dark:text-white dark:bg-gray-800"
               >
-                Batch
+                Academic course code
               </th>
               <td className="px-6 py-4">
                 {location.state.academic_course_code}
@@ -92,7 +66,7 @@ function ExcemptionVerificationCard(state) {
                 scope="row"
                 className="px-6 py-4 font-medium text-pC whitespace-nowrap bg-pLC dark:text-white dark:bg-gray-800"
               >
-                Section
+                Academic course credit
               </th>
               <td className="px-6 py-4 uppercase">
                 {location.state.academic_course_credit}
@@ -103,7 +77,7 @@ function ExcemptionVerificationCard(state) {
                 scope="row"
                 className="px-6 py-4 font-medium text-pC whitespace-nowrap bg-pLC dark:text-white dark:bg-gray-800"
               >
-                Semester
+                Exemption course code
               </th>
               <td className="px-6 py-4 ">{location.state.course_code}</td>
             </tr>
@@ -112,7 +86,7 @@ function ExcemptionVerificationCard(state) {
                 scope="row"
                 className="px-6 py-4 font-medium text-pC whitespace-nowrap bg-pLC dark:text-white dark:bg-gray-800"
               >
-                Semester
+                Exemption course credit
               </th>
               <td className="px-6 py-4 ">{location.state.course_credit}</td>
             </tr>
@@ -121,9 +95,54 @@ function ExcemptionVerificationCard(state) {
                 scope="row"
                 className="px-6 py-4 font-medium text-pC whitespace-nowrap bg-pLC dark:text-white dark:bg-gray-800"
               >
-                Semester
+                Exemption course name
               </th>
               <td className="px-6 py-4 ">{location.state.course_name}</td>
+            </tr>
+            <tr className="border-b border-pMC">
+              <th
+                scope="row"
+                className="px-6 py-4 font-medium text-pC whitespace-nowrap bg-pLC dark:text-white dark:bg-gray-800"
+              >
+                Email
+              </th>
+              <td className="px-6 py-4 ">{location.state.studentEmail}</td>
+            </tr>
+            <tr className="border-b border-pMC">
+              <th
+                scope="row"
+                className="px-6 py-4 font-medium text-pC whitespace-nowrap bg-pLC dark:text-white dark:bg-gray-800"
+              >
+                Batch
+              </th>
+              <td className="px-6 py-4 ">{location.state.studentBatch}</td>
+            </tr>
+            <tr className="border-b border-pMC">
+              <th
+                scope="row"
+                className="px-6 py-4 font-medium text-pC whitespace-nowrap bg-pLC dark:text-white dark:bg-gray-800"
+              >
+                Department
+              </th>
+              <td className="px-6 py-4 ">{location.state.department}</td>
+            </tr>
+            <tr className="border-b border-pMC">
+              <th
+                scope="row"
+                className="px-6 py-4 font-medium text-pC whitespace-nowrap bg-pLC dark:text-white dark:bg-gray-800"
+              >
+                Section
+              </th>
+              <td className="px-6 py-4 ">{location.state.section}</td>
+            </tr>
+            <tr className="border-b border-pMC">
+              <th
+                scope="row"
+                className="px-6 py-4 font-medium text-pC whitespace-nowrap bg-pLC dark:text-white dark:bg-gray-800"
+              >
+                Current Semester
+              </th>
+              <td className="px-6 py-4 ">{location.state.currentSemester}</td>
             </tr>
           </tbody>
         </table>
