a7801ee919974afa33878172c02a309345720004
Arunaa S <arunaasureshkumar@gmail.com>
2023-02-10 17:08:23 +0530

    college offered courses table added

--- src/student/student_home/StudentDashboard.js
+++ src/student/student_home/StudentDashboard.js
@@ -1,105 +1,10 @@
+import NPTELTable from "./NptelTable";
+import CollegeOfferCoursesTable from "./CollegeOfferCoursesTable";
 
 function StudentDashBoard() {
   return (
-    <div className="text-pC " >
-        <div className="h-1/2 flex flex-col justify-evenly">
-            <h1 className="text-xl font-semibold sm:text-2xl md:text-2xl ml-8 md:-ml-12">
-                College Offered Courses
-            </h1>
-            <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-                <table className="w-full text-sm text-left text-pMC m-4 md:m-0 max-w-1/2 min-w-3/5">
-                    <thead className="text-xs text-pLC uppercase bg-pC ">
-                        <tr>
-                            <th scope="col" className="md:px-6 md:py-3 ">
-                                Course name
-                            </th>
-                            <th scope="col" className="md:px-6 md:py-3">
-                                Total Mark %
-                            </th>
-                            <th scope="col" className="md:px-6 md:py-3">
-                                Attendence %
-                            </th>
-                            <th scope="col" className="md:px-6 md:py-3">
-                                Total Credit
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr className="bg-white border-b dark:bg-pC dark:border-pC">
-                            <th scope="row" className="md:px-6 py-4 font-medium text-pC whitespace-nowrap dark:text-white ">
-                                Go Programming
-                            </th>
-                            <td className="md:px-6 py-4">
-                                90 %
-                            </td>
-                            <td className="md:px-6 py-4">
-                                96 %
-                            </td>
-                            <td className="md:px-6 py-4">
-                                2
-                            </td>
-                        </tr>
-                    </tbody>
-                    <tbody>
-                        <tr className="bg-white border-b dark:bg-pC dark:border-pC">
-                            <th scope="row" className="md:px-6 py-4  font-medium text-pC whitespace-nowrap dark:text-white break-words">
-                                Git and Version Control System
-                            </th>
-                            <td className="md:px-6 py-4">
-                                90 %
-                            </td>
-                            <td className="md:px-6 py-4">
-                                96 %
-                            </td>
-                            <td className="md:px-6 py-4">
-                                2
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-        <div className="h-1/2 flex flex-col justify-evenly">
-            <div className="text-xl font-semibold sm:text-2xl md:text-2xl ml-8 md:-ml-12">
-                NPTEL Courses
-            </div>
-            <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-                <table className="w-full text-sm text-left text-pMC ">
-                    <thead className="text-xs text-pLC uppercase bg-pC ">
-                        <tr cl>
-                            <th scope="col" className="md:px-6 md:py-3">
-                                Course name
-                            </th>
-                            <th scope="col" className="md:px-6 md:py-3">
-                                Total Mark %
-                            </th>
-                            <th scope="col" className="md:px-6 md:py-3">
-                                Attendence %
-                            </th>
-                            <th scope="col" className="md:px-6 md:py-3">
-                                Total Credit
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr className="bg-white border-b dark:bg-pC dark:border-pC">
-                            <th scope="row" className="md:px-6 py-4 font-medium text-pC whitespace-nowrap dark:text-white">
-                                Descriptive Statistics with R Software
-                            </th>
-                            <td className="md:px-6 py-4">
-                                Sliver
-                            </td>
-                            <td className="md:px-6 py-4">
-                                Laptop
-                            </td>
-                            <td className="md:px-6 py-4">
-                                3
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>
+    <div className="text-pC ">
+      <CollegeOfferCoursesTable />
     </div>
   );
 }
---
+++ src/student/student_home/collegeOfferCoursesTable.js
@@ -0,0 +1,57 @@
+export default function CollegeOfferCoursesTable() {
+  return (
+    <div className="text-pC ">
+      <div className="h-1/2 flex flex-col justify-evenly">
+        <h1 className="text-xl font-semibold sm:text-2xl md:text-2xl ml-8 md:-ml-12">
+          College Offered Courses
+        </h1>
+        <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+          <table className="w-full text-sm text-left text-pMC m-4 md:m-0 max-w-1/2 min-w-3/5">
+            <thead className="text-xs text-pLC uppercase bg-pC ">
+              <tr>
+                <th scope="col" className="md:px-6 md:py-3 ">
+                  Course name
+                </th>
+                <th scope="col" className="md:px-6 md:py-3">
+                  Total Mark %
+                </th>
+                <th scope="col" className="md:px-6 md:py-3">
+                  Attendence %
+                </th>
+                <th scope="col" className="md:px-6 md:py-3">
+                  Total Credit
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="bg-white border-b text-pC">
+                <th
+                  scope="row"
+                  className="md:px-6 py-4 font-medium  whitespace-nowrap  "
+                >
+                  Go Programming
+                </th>
+                <td className="md:px-6 py-4">90 %</td>
+                <td className="md:px-6 py-4">96 %</td>
+                <td className="md:px-6 py-4">2</td>
+              </tr>
+            </tbody>
+            <tbody>
+              <tr className="bg-white border-b text-pC">
+                <th
+                  scope="row"
+                  className="md:px-6 py-4  font-medium text-pC whitespace-nowrap  break-words"
+                >
+                  Git and Version Control System
+                </th>
+                <td className="md:px-6 py-4">90 %</td>
+                <td className="md:px-6 py-4">96 %</td>
+                <td className="md:px-6 py-4">2</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+}
